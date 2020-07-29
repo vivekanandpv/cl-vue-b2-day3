c9ba2158e35a3d3620c46fbd7e1b1954c226a5bd
Vivekanand P V <vivekanandpv@outlook.com>
2020-07-29 22:31:25 +0530

    done

--- src/components/Http.vue
+++ src/components/Http.vue
@@ -10,6 +10,7 @@ export default {
     try {
       //  since the http client is configured for global injector,
       //  we can use it as part of every child
+      //  More info: https://github.com/axios/axios#axios-api
       const data = await this.$http.get(`users?page=2`);
       console.log('Server', data);
     } catch (error) {
