b7c33b044de09126b5aa33e088f4e2335a675cbd
Vivekanand P V <vivekanandpv@outlook.com>
2020-07-29 20:50:25 +0530

    done

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,30 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import Home from '../components/Home';
+import About from '../components/About';
+import Contact from '../components/Contact';
+import News from '../components/News';
 
 Vue.use(VueRouter);
 
-const routes = [];
+const routes = [
+  {
+    path: '/',
+    component: Home,
+  },
+  {
+    path: 'about',
+    component: About,
+  },
+  {
+    path: 'contact',
+    component: Contact,
+  },
+  {
+    path: 'news',
+    component: News,
+  },
+];
 
 const router = new VueRouter({
   mode: 'history',
