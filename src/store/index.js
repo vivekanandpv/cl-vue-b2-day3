e003cc680a593cf83ae4468b9ca703f8068ee47e
Vivekanand P V <vivekanandpv@outlook.com>
2020-07-29 22:15:58 +0530

    done

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,36 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    likes: 0,
+    dislikes: 0,
   },
   mutations: {
+    like(state, count) {
+      state.likes += count;
+    },
+    dislike(state, count) {
+      state.dislikes += count;
+    },
   },
   actions: {
+    //  mutation is abstracted
+    //  actions can be asynchronous
+    //  promise chaining can be used
+    //  to create composite actions
+    //  with complex async workflows
+    likeAsync(context, count) {
+      setTimeout(() => {
+        context.commit('like', count);
+      }, 250);
+    },
+    dislikeAsync(context, count) {
+      setTimeout(() => {
+        context.commit('dislike', count);
+      }, 250);
+    },
   },
-  modules: {
-  }
-})
+});
