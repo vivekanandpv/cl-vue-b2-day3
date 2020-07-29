f73c211c0cb387cedc3d4474eeb475cf5a8962fb
Vivekanand P V <vivekanandpv@outlook.com>
2020-07-29 21:08:35 +0530

    done

--- src/components/News.vue
+++ src/components/News.vue
@@ -1,9 +1,21 @@
 <template>
-  <h3>News</h3>
+  <div>
+    <h3>News</h3>
+    <p>City: {{city}} Year: {{year}}</p>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data: function () {
+    return {
+      //  this refers to the current instance of the component
+      //  $route is the application-wide singleton injected by Vue
+      city: this.$route.params.city,
+      year: this.$route.params.year,
+    };
+  },
+};
 </script>
 
 <style>
