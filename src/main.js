bd2b1747269b9a60672e9ff5993366dab06e4fa5
Vivekanand P V <vivekanandpv@outlook.com>
2020-07-29 22:29:25 +0530

    done

--- src/main.js
+++ src/main.js
@@ -1,12 +1,21 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import { http } from './lib/http';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+//  manually register the http client
+//  for global injector as a singleton
+Vue.use({
+  install(Vue) {
+    Vue.prototype.$http = http;
+  },
+});
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount('#app');
